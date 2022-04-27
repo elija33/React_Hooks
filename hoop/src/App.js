f1828915ea5f1a751df0cdbba941b4c0f0edc1d2
Elija Amponsa <amponseli@gmail.com>
2022-04-26 22:10:23 -0400

    HookCounterOne, HookMouse and MouseContainer

--- hoop/src/App.js
+++ hoop/src/App.js
@@ -3,8 +3,11 @@ import './App.css'
 import ClassCounter from './components/ClassCounter'
 import HookCounter from './components/HookCounter'
 import HookCounterFour from './components/HookCounterFour'
+import HookCounterOne from './components/HookCounterOne'
 import HookCounterThree from './components/HookCounterThree'
 import HookCounterTwo from './components/HookCounterTwo'
+import HookMouse from './components/HookMouse'
+import MouseContainer from './components/MouseContainer'
 
 function App() {
   return (
@@ -12,8 +15,11 @@ function App() {
      {/* <ClassCounter /> 
      <HookCounter />
      <HookCounterTwo />
-     <HookCounterThree />*/}
+     <HookCounterThree />
      <HookCounterFour />
+     <HookCounterOne />
+     <HookMouse />*/}
+     <MouseContainer />
     </div>
   )
 }
