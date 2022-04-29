a363fd0b833a642052ae59971d13d3871fb50376
Elija Amponsa <amponseli@gmail.com>
2022-04-29 16:32:44 -0400

    UseContext, useReducer, and Fetching

--- hoop/src/App.js
+++ hoop/src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
 import './App.css'
 import ClassCounter from './components/ClassCounter'
+import CounterOne from './components/CounterOne'
+import CounterTwo from './components/CounterTwo'
+import DataFetching from './components/DataFetching'
 import HookCounter from './components/HookCounter'
 import HookCounterFour from './components/HookCounterFour'
 import HookCounterOne from './components/HookCounterOne'
 import HookCounterThree from './components/HookCounterThree'
 import HookCounterTwo from './components/HookCounterTwo'
 import HookMouse from './components/HookMouse'
+import IntervalHookCounter from './components/IntervalHookCounter'
 import MouseContainer from './components/MouseContainer'
 
 function App() {
@@ -18,8 +22,12 @@ function App() {
      <HookCounterThree />
      <HookCounterFour />
      <HookCounterOne />
-     <HookMouse />*/}
+     <HookMouse />
      <MouseContainer />
+     <IntervalHookCounter />
+     <DataFetching />
+     <CounterOne />*/}
+     <CounterTwo />
     </div>
   )
 }
